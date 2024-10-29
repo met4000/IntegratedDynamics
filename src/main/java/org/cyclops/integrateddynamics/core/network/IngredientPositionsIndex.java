bdef44d801a282d0de03af5c33c72b023654b537
Ruben Taelman <rubensworks@gmail.com>
2024-10-29 18:47:22 +0100

    Revert "Optimize ingredient positions index"
    This reverts commit 61b7372bda5cde123a2b320ac92c1c24d2dfb9b1.
    
    Closes CyclopsMC/IntegratedTerminals#134

--- src/main/java/org/cyclops/integrateddynamics/core/network/IngredientPositionsIndex.java
+++ src/main/java/org/cyclops/integrateddynamics/core/network/IngredientPositionsIndex.java
@@ -9,7 +9,6 @@ import org.cyclops.cyclopscore.ingredient.collection.IIngredientCollapsedCollect
 import org.cyclops.cyclopscore.ingredient.collection.IIngredientMapMutable;
 import org.cyclops.cyclopscore.ingredient.collection.IngredientCollectionHelpers;
 import org.cyclops.cyclopscore.ingredient.collection.IngredientHashMap;
-import org.cyclops.cyclopscore.ingredient.collection.IngredientMapSingleClassified;
 import org.cyclops.integrateddynamics.api.ingredient.IIngredientPositionsIndex;
 import org.cyclops.integrateddynamics.api.part.PartPos;
 import org.cyclops.integrateddynamics.api.part.PrioritizedPartPos;
@@ -67,7 +66,7 @@ public class IngredientPositionsIndex<T, M> implements IIngredientPositionsIndex
     public void addPosition(T instance, PrioritizedPartPos pos) {
         IIngredientMapMutable<T, M, ObjectOpenHashSet<PartPos>> positionsMap = this.prioritizedPositionsMap.get(getInternalPriority(pos));
         if (positionsMap == null) {
-            positionsMap = createCollapsedMap(getComponent());
+            positionsMap = new IngredientHashMap<>(getComponent());
             this.prioritizedPositionsMap.put(getInternalPriority(pos), positionsMap);
         }
 
@@ -79,17 +78,6 @@ public class IngredientPositionsIndex<T, M> implements IIngredientPositionsIndex
         }
 
         set.add(pos.getPartPos());
-    }
-
-    // If this would ever be needed in other places as well, let's move this to IngredientCollectionHelpers
-    protected static <T, M> IIngredientMapMutable<T, M, ObjectOpenHashSet<PartPos>> createCollapsedMap(IngredientComponent<T, M> ingredientComponent) {
-        if (ingredientComponent.getCategoryTypes().size() == 1) {
-            return new IngredientHashMap<>(ingredientComponent);
-        }
-        return new IngredientMapSingleClassified<>(
-                ingredientComponent,
-                () -> new IngredientHashMap<>(ingredientComponent),
-                ingredientComponent.getCategoryTypes().get(0));
     }
 
     @Override
